0be68f8d7e3e5284123427cf058b2c348ce2788b
SaikatLodh <saikatlodh8@gmail.com>
2025-09-12 00:37:01 +0530

    change redis url

--- server/app/config/redis.js
+++ server/app/config/redis.js
@@ -2,23 +2,8 @@ const Redis = require("ioredis");
 const env = require("dotenv");
 env.config({ path: ".env" });
 
-const redis = new Redis({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
-  username: process.env.REDIS_USERNAME,
-  password: process.env.REDIS_PASSWORD,
-});
-
-redis.on("error", (error) => console.error(error));
+const redis = new Redis(process.env.REDIS_URL); // just one env var
 
 redis.on("connect", () => console.log("Redis connected"));
-
-redis.on("disconnect", () => console.log("Redis disconnected"));
-
-redis.on("reconnecting", () => console.log("Redis reconnecting"));
-
-redis.on("end", () => console.log("Redis connection closed"));
-
-redis.on("close", () => console.log("Redis connection closed"));
-
+redis.on("error", (err) => console.error("Redis error", err));
 module.exports = redis;
